8cc6415119fdb41ba9f7f57639f23ca64fd4200a
rodsimonc <99926654+rodsimonc@users.noreply.github.com>
2026-09-04 05:10:35 -0300

    Delete apis directory

--- apis/soap/client.py
+++
@@ -1,6 +0,0 @@
-from zeep import Client
-
-# zeep lee el WSDL y arma el cliente solo
-client = Client("http://127.0.0.1:8007/cotiz?wsdl")
-valor = client.service.GetCotizacion(moneda="USD")
-print("valor devuelto:", valor)
